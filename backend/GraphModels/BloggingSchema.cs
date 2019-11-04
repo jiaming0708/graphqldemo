fdb78742cf4b50a6d84a28b82bcf745316f97867
Jimmy <jiaming0708@gmail.com>
2019-11-04 21:41:11 +0800

    define schema

---
+++ backend/GraphModels/BloggingSchema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GraphQL.Types;
+using GraphQL.Utilities;
+
+namespace backend.GraphModels
+{
+  public class BloggingSchema : Schema
+  {
+    public BloggingSchema(IServiceProvider provider)
+            : base(provider)
+    {
+      Query = provider.GetRequiredService<BloggingQuery>();
+    }
+  }
+}
