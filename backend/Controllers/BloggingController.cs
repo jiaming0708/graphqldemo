18682bb4078157a25df2ca606fc25ecf978ed8e9
Jimmy <jiaming0708@gmail.com>
2019-11-04 21:32:53 +0800

    write an api for sure EF working

---
+++ backend/Controllers/BloggingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using backend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class BloggingController : ControllerBase
+  {
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IPostRepository _postRepository;
+
+    public BloggingController(ILogger<WeatherForecastController> logger, IPostRepository postRepository)
+    {
+      _logger = logger;
+      _postRepository = postRepository;
+    }
+
+    [HttpGet]
+    public IEnumerable<Post> Get()
+    {
+      return _postRepository.GetPosts().ToList();
+    }
+  }
+}
