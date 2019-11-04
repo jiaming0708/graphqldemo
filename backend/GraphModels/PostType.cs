478a188ff850820df791c8360c811ac22db9d400
Jimmy <jiaming0708@gmail.com>
2019-11-04 21:40:32 +0800

    define type

---
+++ backend/GraphModels/PostType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using backend.Models;
+using backend.Repositories;
+using GraphQL.Types;
+
+namespace backend.GraphModels
+{
+  public class PostType : ObjectGraphType<Post>
+  {
+    public PostType(IUserRepository userRepository)
+    {
+      Field(p => p.Id).Description("Post id");
+      Field(p => p.Title).Description("Post title");
+      Field(p => p.Context).Description("Post context");
+      Field<UserType>("Author", resolve: context => userRepository.GetUserById(context.Source.AuthorId));
+    }
+  }
+}
---
+++ backend/GraphModels/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using backend.Models;
+using GraphQL.Types;
+
+namespace backend.GraphModels
+{
+  public class UserType : ObjectGraphType<User>
+  {
+    public UserType()
+    {
+      Field(p => p.Id).Description("User id");
+      Field(p => p.Name).Description("User name");
+    }
+  }
+}
