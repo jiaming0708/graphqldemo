3c6e55b50e31dcacb87e006721ee8ed1bc296ed7
Jimmy <jiaming0708@gmail.com>
2019-11-04 21:25:44 +0800

    setup db connection

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Models;
+using backend.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,8 +27,13 @@ namespace backend
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+    {
+      // repository
+      services.AddScoped<IPostRepository, PostRepository>();
+      services.AddScoped<IUserRepository, UserRepostiory>();
+      services.AddDbContext<BloggingContext>(options => options.UseSqlite(Configuration.GetConnectionString("Database")));
+
+      services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
