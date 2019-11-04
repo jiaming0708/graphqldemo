d22e062ed67f662da471d52378c361938f7eb5d9
Jimmy <jiaming0708@gmail.com>
2019-11-04 21:41:02 +0800

    define query

---
+++ backend/GraphModels/BloggingQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using backend.Repositories;
+using GraphQL.Types;
+
+namespace backend.GraphModels
+{
+  public class BloggingQuery : ObjectGraphType
+  {
+    public BloggingQuery(IPostRepository postRepository, IUserRepository userRepository)
+    {
+      Field<ListGraphType<PostType>>(
+        "posts",
+        arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id", Description = "post id" }),
+        resolve: context => {
+          var id = context.GetArgument<int>("id");
+          if (id == 0)
+          {
+            return postRepository.GetPosts();
+          }
+
+          return postRepository.GetPostById(id);
+        }
+        );
+
+      Field<ListGraphType<UserType>>(
+        "users",
+        arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id", Description = "user id" }),
+        resolve: context =>
+        {
+          var id = context.GetArgument<int>("id");
+          if (id == 0)
+          {
+            return userRepository.GetUsers();
+          }
+
+          return userRepository.GetUserById(id);
+        }
+        );
+    }
+  }
+}
