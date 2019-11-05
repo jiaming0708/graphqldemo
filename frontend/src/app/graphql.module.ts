e258003f74d5b2a17146f1debd93b9e4095b8a77
Jimmy <jiaming0708@gmail.com>
2019-11-05 14:19:27 +0800

    setup the server url

--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = 'https://localhost:5001/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
