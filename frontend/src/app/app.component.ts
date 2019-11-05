0fba811a1b7b952ba020744b64fe8f5045a9749a
Jimmy <jiaming0708@gmail.com>
2019-11-05 14:23:35 +0800

    query data and show table on client

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  constructor(private apollo: Apollo) {
+  }
+  post$: Observable<any>;
+
+  ngOnInit() {
+    this.post$ = this.apollo.watchQuery({
+    query: gql`{
+      posts{
+        id,
+        title,
+        context,
+        author{
+          id,
+          name
+        }
+      }
+    }`}).valueChanges;
+  }
 }
